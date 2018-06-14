a753e963ef3762ea4b4034bdc69540abebb131f9
Simon Massey <simon@simonmassey.org>
2018-06-14 21:20:06 +0100

    needs  for better automation.

--- utils/brew/brew.sh
+++ utils/brew/brew.sh
@@ -10,22 +10,38 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-git clone git@github.com:simbo1905/homebrew-core.git
-pushd homebrew-core
-git checkout -b "$VERSION" 
-REPLACE=$(find Formula -type f -name git-secret.rb)
+if [ -z "$REPO" ]; then
+    (>&2 echo "ERROR please provide REPO")
+    exit 1
+fi
 
-echo "REPLACE=$REPLACE"
+TEMPLATE=$(realpath $(find . -type f -name git-secret.rb.template))
+
+pushd $(brew --repository homebrew/core)
+
+git checkout -b "$VERSION"
+
+REPLACE=$(find Formula -type f -name git-secret.rb)
 
 ARCHIVE="https://github.com/sobolevn/git-secret/archive/${VERSION}.tar.gz"
 
 SHA256=$(wget -q -O - $ARCHIVE | shasum -a 256 | awk '{print $1}')
 
-a='s/^  sha256 .*/  sha256 "'
-b='"/1'
+shaStart='s/^  sha256 .*/  sha256 "'
+shaEnd='"/1'
 
-cat ../git-secret.rb | sed "$a$SHA256$b" > $REPLACE
+urlStart='s/^\(  url "https:..github.com.sobolevn.git-secret.archive.\).*.tar.gz"/\1'
+urlEnd='.tar.gz"/1'
+
+cat $TEMPLATE | sed "$shaStart$SHA256$shaEnd" | sed "$urlStart$VERSION$urlEnd" > $REPLACE
+
+#brew tests
+brew install --build-from-source git-secret
+brew test git-secret --devel
+brew audit --strict git-secret
+
 git add $REPLACE
-git commit -m "update git-secret to version $VERSION"
-git push origin "$VERSION" 
+git commit -m "git-secret $VERSION"
+git push git@github.com:$REPO/homebrew-core.git "$VERSION"
+
 popd
